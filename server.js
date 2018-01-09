0426975b0e09737a450b000fa328fa6bb25a15bf
mzastue <mzastue@gmail.com>
2018-01-09 21:03:15 +0100

    Serve files for heroku

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const history = require('connect-history-api-fallback');
 const PORT = process.env.PORT || 8080;
+const staticFileMiddleware = express.static(__dirname + '/dist');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true,
+}));
+
+app.use(staticFileMiddleware);
 
 app.listen(PORT);
 
